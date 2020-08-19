2fabed41854a2b159ad71196da8af2e3d08e465e
Telly Lowe <57466776+TellyLowe@users.noreply.github.com>
2020-08-19 16:46:23 -0400

    TabNav & options

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Entypo } from "@expo/vector-icons";
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
@@ -10,15 +12,52 @@ import ProfileScreen from "./components/screens/ProfileScreen";
 const AppStack = createStackNavigator();
 const TabNav = createBottomTabNavigator();
 
+const tabBarOptions = {
+  showlabel: false,
+  style: {
+    backgroundColor: "343434",
+    borderTopColor: "343434",
+    paddingBottom: 12,
+  },
+};
+
 const TabNavScreen = () => {
   return (
-    <TabNav.Navigator>
+    <TabNav.Navigator
+          tabBarOptions={tabBarOptions}
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused }) => {
+                    let iconName;
+
+                    switch (route.name) {
+                        case "HomeScreen":
+                            iconName = "home";
+                            break;
+                        case "LiveScreen":
+                            iconName = "game-controller";
+                            break;
+                        case "ProfileScreen":
+                            iconName = "user";
+                            break;
+
+                        default:
+                            iconName = "home";
+                            break;
+                    }
+                    return (
+                      <TabBarIconContainer focused={focused}>
+                            <Entypo name={iconName} size={24} color="#ffffff" />
+                      </TabBarIconContainer>
+                    );
+                  },
+              })}
+            >
       <TabNav.Screen name="HomeScreen" component={HomeScreen} />
       <TabNav.Screen name="LiveScreen" component={LiveScreen} />
       <TabNav.Screen name="ProfileScreen" component={ProfileScreen} />
     </TabNav.Navigator>
-  )
-}
+  );
+};
 
 export default function App() {
   return (
@@ -29,5 +68,11 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+const TabBarIconContainer = styled.View`
+    background-color: ${(props) => (props.focused ? "#819ee5" : "#343434")};
+    padding: 2px 16px;
+    border-radius: 32px;
+`;
 
 
