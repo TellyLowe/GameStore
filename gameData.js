93000776f838ae2a4aef06b181f7f208023a2e77
Telly Lowe <57466776+TellyLowe@users.noreply.github.com>
2020-08-19 13:39:47 -0400

    GameData

---
+++ gameData.js
@@ -0,0 +1,62 @@
+export default games = [
+    {
+        id: 1,
+        title: "Unforeseen Incidents",
+        teaser: "A classical style interactive mystery set in a beautifully hand-painted world.",
+        description:
+            "Unforeseen Incidents is a classifcal style interactive mystery set in a beautifully hand-painted world",
+        rating: 4.5,
+        category: ["Action", "Indie"],
+        age: "12+",
+        cover: require("./assets/un1.webp"),
+        screenshots: [
+            require("./assets/un2.jfif"),
+            require("./assets/un3.jpg"),
+            require("./assets/un4.jpg"),
+            require("./assets/un5.jpg"),
+            require("./assets/un6.jpg"),
+        ],
+
+        backgroundColor: "#824571",
+
+    },
+    {
+        id: 2,
+        title: "Ori and the Will of the Wisps",
+        teaser: "Embark on a new journey in a vast, exotic world where you’ll encounter towering enemies.",
+        description:
+            "Play the critically acclaimed masterpiece. Embark on a new journey in a vast, exotic world where you’ll encounter towering enemies and challenging puzzles on your quest to unravel Ori’s destiny.",
+        rating: 3.8,
+        category: ["Strategy", "Indie", "Action"],
+        age: "11+",
+        cover: require("./assets/ow1.jpg"),
+        screenshots: [
+            require("./assets/ow2.jfif"),
+            require("./assets/ow3.jfif"),
+            require("./assets/ow4.jpg"),
+            require("./assets/ow5.jfif"),
+            require("./assets/ow6.jfif"),
+            require("./assets/ow7.jpg"),
+            require("./assets/ow8.jpg"),
+        ],
+        backgroundColor: "#456382",
+    },
+    {
+        id: 3,
+        title: "Unravel",
+        teaser: "Yarny, a character made from a single thread of yarn.",
+        description:
+            "Unravel is a physics-based puzzle platformer. Yarny, a character made from a single thread of yarn, must embark on a larger than life adventure.",
+        rating: 2.4,
+        category: ["Casual", "Indie", "Adventure"],
+        age: "13+",
+        cover: require("./assets/ur1.jpg"),
+        screenshots: [
+            require("./assets/ur2.jpg"),
+            require("./assets/ur3.jpg"),
+            require("./assets/ur4.jpg"),
+            require("./assets/ur5.jpg"),
+        ],
+        backgroundColor: "#825D45",
+    },
+];
