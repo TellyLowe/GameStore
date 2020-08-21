10443a9d3493cd6d97450d92e5c57eb4a0c9fb06
Telly Lowe <57466776+TellyLowe@users.noreply.github.com>
2020-08-21 18:47:53 -0400

    GameScreen fin
    added rating & thumb

--- components/screens/GameScreen.js
+++ components/screens/GameScreen.js
@@ -1,12 +1,31 @@
 import React from "react";
 import { StatusBar } from "react-native";
-import styled from 'styled-components';
+import styled from "styled-components";
 import { Ionicons } from "@expo/vector-icons";
 
 import Text from "../Text";
 
 export default GameScreen = ({ route, navigation }) => {
     const { game } = route.params;
+
+    const renderStars = () => {
+        let stars = [];
+
+        for (let s = 1; s <= 5; s++) {
+            stars.push(
+                <Ionicons
+                    key={s}
+                    name="ios-star"
+                    size={16}
+                    style={{ marginRight: 5 }}
+                    color={Math.floor(game.rating) >= s ? "#819ee5" : "#434958"}
+                />
+            );
+        }
+
+        return <Stars>{stars}</Stars>;
+    };
+
     return (
         <GameContainer>
             <StatusBar barStyle="light-content" />
@@ -17,7 +36,53 @@ export default GameScreen = ({ route, navigation }) => {
                     <Ionicons name="ios-close" size={48} color="#ffffff" />
                 </BackButton>
             </GameArtContainer>
-            <Text>Game Screen</Text>
+
+            <GameInfoContainer>
+                <GameThumbContainer>
+                    <GameThumb source={game.cover} />
+                </GameThumbContainer>
+                <GameInfo>
+                    <Text heavy medium>
+                        {game.title}
+                    </Text>
+                    <Text color="#9a9a9a">{game.teaser}</Text>
+                </GameInfo>
+                <Download>
+                    <Ionicons name="md-cloud-download" size={24} color="#ffffff" />
+                </Download>
+            </GameInfoContainer>
+
+            <GameStatsContainer>
+                {renderStars()}
+                <Text heavy color="#9a9a9a">
+                    {game.rating}
+                </Text>
+                <Text bold color="#9a9a9a">
+                    {game.category[0]}
+                </Text>
+                <Text bold color="#9a9a9a">
+                    {game.age}
+                </Text>
+                <Text bold color="#9a9a9a">
+                    Game of the day
+                </Text>
+            </GameStatsContainer>
+
+            <ScreenShotsContainer>
+                <ScreenShots horizontal={true} showsHorizontalScrollIndicator={false}>
+                    {game.screenshots.map((screenshot, index) => {
+                        return (
+                            <ScreenShotContainer key={index}>
+                                <ScreenShot source={screenshot} />
+                            </ScreenShotContainer>
+                        );
+                    })}
+                </ScreenShots>
+            </ScreenShotsContainer>
+
+            <Description medium color="#9a9a9a">
+                {game.description}
+            </Description>
         </GameContainer>
     );
 };
@@ -40,6 +105,76 @@ const GameArt = styled.Image`
 
 const BackButton = styled.TouchableOpacity`
     position: absolute;
-    top: 40px;
-    left: 10px;
+    top: 48px;
+    left: 16px;
+`;
+
+const GameInfoContainer = styled.View`
+    flex-direction: row;
+    align-items: center;
+    padding: 16px 0;
+    margin: 8px 16px;
+`;
+
+const GameThumbContainer = styled.View`
+    shadow-color: #000000;
+    shadow-offset: 1px 1px;
+    shadow-opacity: 0.5;
+    shadow-radius: 2px;
+`;
+
+const GameThumb = styled.Image`
+    width: 80px;
+    height: 80px;
+    border-radius: 16px;
+`;
+
+const GameInfo = styled.View`
+    width: 0;
+    flex-grow: 1;
+    margin: 0 8px 0 16px;
+`;
+
+const Download = styled.View`
+    background-color: #819ee5;
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    align-items: center;
+    justify-content: center;
+`;
+
+const GameStatsContainer = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 0 16px;
+`;
+
+const Stars = styled.View`
+    flex-direction: row;
+`;
+
+const ScreenShotsContainer = styled.View`
+    margin: 8px 0;
+`;
+
+const ScreenShots = styled.ScrollView``;
+
+const ScreenShotContainer = styled.View`
+    padding: 16px;
+    shadow-color: #000000;
+    shadow-offset: 1px 1px;
+    shadow-opacity: 0.5;
+    shadow-radius: 5px;
+`;
+
+const ScreenShot = styled.Image`
+    height: 200px;
+    width: 300px;
+    border-radius: 12px;
+`;
+
+const Description = styled(Text)`
+    margin: 0 16px;
+    line-height: 22px;
 `;
